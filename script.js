2e3613e05b7b2ce990e7c418ba79169bdd23d127
Devendragupta96 <devendragupta1302@gmail.com>
2026-01-19 10:20:28 +0530

    our first javascript code

---
+++ script.js
@@ -0,0 +1,120 @@
+// console.log("Helo welcome to the advanced class")
+
+
+
+
+// let obj={
+//     name:"Devendra",
+//     location:"Gurgaon",
+//     age:23
+// }
+// let obj2={
+//     name:"Devendra",
+//     location:"Gurgaon",
+//     age:23
+// }
+
+// obj.age=66
+// console.log(obj)
+
+// const para=document.querySelector("p")
+// para.textContent="this is updated one"
+// // console.log(para)
+
+// console.info(document)
+
+// let obj={
+//     name:"Devendra",
+//     phone:65754645644,
+//     address:"GGN"
+// }
+
+// let obj2={
+//     name:"Alex",
+//     phone:65754645644,
+//     address:"GGN"
+// }
+
+// obj2.name="Bob"
+// console.log(obj2.name)
+
+// const para=document.getElementById('para')
+// // para.textContent="This is now updated paragraph"
+// para.style.color="green"
+// console.log(para)
+
+// const para=document.getElementById('para')
+// // para.textContent="This is now updated paragraph"
+// para.style.color="green"
+// console.log(para)// const para=document.getElementById('para')
+
+// const para=document.getElementsByClassName('para')
+// para[0].textContent="This is now updated paragraph"
+// para[1].style.color="green"
+// console.log(para)
+// para.forEach((ele)=>console.log(ele.textContent))
+
+// const para2=document.querySelectorAll('.para')
+// para2[0].textContent="This is now updated paragraph"
+// para2[1].style.color="green"
+// console.log(para2)
+// para2.forEach((ele)=>console.log(ele.textContent))
+
+
+// const para=document.querySelector('.para')
+// para.textContent="This is now updated paragraph"
+// para.style.color="green"
+// console.log(para)
+
+
+// let arr=[12,4,65,3,324,56]
+
+// arr.forEach((ele)=>console.log(ele))
+
+// const para2=document.querySelectorAll('.para')
+// para2[0].innerText="This is now updated paragraph" //does not work on hidden text
+// para2[0].textContent="This is now updated paragraph"
+// para2[1].style.color="green"
+// para2[1].innerHTML="<div><b>Hello</b> World</div>"
+// console.log(para2)
+
+
+// const button=document.querySelector('button')
+
+// button.classList.add('btn')
+// // button.classList.remove('btn')
+// // button.classList.toggle('btn')
+
+// function message(event){
+//     console.log(event)
+//     alert("You have clicked the button")
+// }
+// button.addEventListener('click',message)
+// button.removeEventListener('click',message)
+
+// const button=document.querySelector('#btn')
+// const button2=document.querySelector('#stop')
+
+// button.addEventListener('click',message)
+
+// button2.addEventListener('click',function(){
+//     button.removeEventListener('click',message)
+// })
+
+
+// const btn=document.querySelector("#btn")
+// btn.addEventListener('keydown',function(event){
+//     console.log(event.key)
+// })
+
+// btn.addEventListener('keyup',function(event){
+//     console.log(event.key)
+// })
+
+const handleSubmit=(event)=>{
+    event.preventDefault()
+    console.log("Form submitted")
+}
+
+const form=document.querySelector('form')
+form.addEventListener('submit',handleSubmit)
