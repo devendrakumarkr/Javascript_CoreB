14ed672c3953f5c9735b14678ba3f0de7fcf0b55
devendrakumarkr <devendra.kumar@krmangalam.edu.in>
2026-04-13 10:31:34 +0530

    feat added tailwind css code

--- reactapp/src/component/Sample.jsx
+++ reactapp/src/component/Sample.jsx
@@ -30,20 +30,23 @@ function Sample(props) {
     <div>
         <Button >First</Button>
         <Button green>Second</Button>
-        <button css={{
-            backgroundColor:"yellow",
-            color:"white",
-            width:"100px",
-            height:"35px",
-            borderRadius:"10px",
-            border:"none"
-        }}>Third</button>
+        <button css={buttonStyle}>Third</button>
+        <button className='bg-red-500 text-white w-20 h-8'>Fourth</button>
     </div>
     </div>
   )
 }
 
 export default Sample
+
+const buttonStyle=css`
+    background-color: orange;
+    color: white;
+    width: 100px;
+    height: 35px;
+    border-radius: 10px;
+    border:none
+`
 
 const Button=styled.button`
     background-color: ${(props) => (props.green ? "green" : "red")};
