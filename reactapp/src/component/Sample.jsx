e5ebdd42d3ddc50d53102fee7a8037c29cedd20e
devendrakumarkr <devendra.kumar@krmangalam.edu.in>
2026-04-20 10:29:52 +0530

    feat added useEffect and fetching api code

--- reactapp/src/component/Sample.jsx
+++ reactapp/src/component/Sample.jsx
@@ -1,19 +1,52 @@
 
-import React,{Component, useState} from 'react'
+import React,{Component, useState, useEffect} from 'react'
 import style from '../css/Sample.module.css'
 import style1 from '../css/HomePage.module.css'
 // import styled from 'styled-components'
 import styled from '@emotion/styled'
 import { css } from '@emotion/react'
+import axios from 'axios'
 
 function Sample(props) {
     const [name,setName]=useState("Devendra")
     const [age,setAge]=useState(20)
+    const [loading, setLoading] = useState(false)
 
+    useEffect(()=>{
+        
+        console.log("Component mounted")
+        return ()=>{
+            console.log("Component unmounted")
+        }
+    },[name])
+
+    useEffect(()=>{
+        // async function fetchingData(){
+        //     const response=await fetch("https://dummyjson.com/products",{
+        //         method:"GET"
+        //     })
+        //     const data=await response.json()
+        //     console.log(data)
+        // }
+
+        async function fetchingData(){
+            try{
+                setLoading(true)
+                const response=await axios.get("https://dummyjson.com/products/")
+            console.log(response.data)
+            setLoading(false)
+            }catch(error){
+                console.log(error)
+            }
+            }
+        
+        fetchingData()
+        },[])
     
 
   return (
     <div>
+        {loading?<h1>Loading...</h1>:<h1></h1>}
       <h2>Trending topics</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quae maiores, nobis laboriosam non commodi quia consequuntur vitae dolore facere deserunt quas recusandae iste harum in, provident aut reprehenderit temporibus.</p>
       <h3>{name}</h3>
